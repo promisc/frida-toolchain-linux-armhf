c178faa73b7d831910c8e82e1e6b589be3a809b2
promisc <97604133+promisc@users.noreply.github.com>
2022-01-20 15:34:02 +0000

    fix more generic

--- build-log-error.sh
+++ build-log-error.sh
@@ -1,17 +1,11 @@
 #!/bin/bash -ux
 
 get_logs () {
+    echo -en '\n\n\n\n================== Logs below ====================\n\n\n\n'
     find build -type f -name "*.log" -printf "%T@:%p\n" | sort -n | cut -d ":" -f 2 | xargs -rL 1 -- tail -vn 100
     exit 1
 }
 
-make -f Makefile.toolchain.mk || get_logs
-# outputs: build/toolchain-linux-armhf.tar.bz2
-
 find build -type f -name "*.log" -exec rm {} +
-make -f Makefile.sdk.mk || get_logs
-# outputs: build/sdk-linux-armhf.tar.bz2
 
-find build -type f -name "*.log" -exec rm {} +
-make core-linux-armhf || get_logs
-# RUN make check-core-linux-armhf
+$@ || get_logs
